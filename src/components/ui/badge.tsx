61ad4afc412b21c4bae2084b6cfaf41419947c13
Aaronrabanal24 <aaronjrabanal@gmail.com>
2025-10-06 21:32:34 +0000

    Fix ESLint namespace import error and add missing dependencies
    - Replace namespace import with named import in badge component
    - Install undici as dev dependency for test setup
    - Install date-fns for date formatting in FunnelCard
    - Update interface to use imported HTMLAttributes type

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import * as React from 'react'
+import { HTMLAttributes } from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
@@ -25,7 +25,7 @@ const badgeVariants = cva(
 )
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
+  extends HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
